4fabce0e50b29908e8617e22f32f3beafe83795d
ashwantkumar1007 <ashwantkumar1007@gmail.com>
2022-04-20 15:31:22 +0530

    first commit

---
+++ SimpleCalculator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SimpleCalculator{
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		int a,b,sum=0;
+		int b;
+		System.out.println("Enter first number");
+		a=scan.nextInt();
+		System.out.println("Enter Second number");
+		b=scan.nextInt();
+		sum=a+b;
+		System.out.println("The sum of Two Number is:"+sum);
+	}	
+}
